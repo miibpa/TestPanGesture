67c606f089915ab7bd306fdbde58215cee808202
Miguel Ibañez Paricio <mibanepa@mercadona.es>
2021-03-05 00:08:34 +0100

    Remove redundancy

--- TestPanGesture/ViewController.swift
+++ TestPanGesture/ViewController.swift
@@ -13,6 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         //let view = CalendarView()
-        self.view.addSubview(CalendarView())
+        //self.view.addSubview(CalendarView())
     }
 }
